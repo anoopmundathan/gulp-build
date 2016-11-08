f3fc3f524dbb60e410b30f8e72cf54207afa87cb
anoopmundathan <anoop.mundathan@gmail.com>
2016-11-08 23:24:08 +0000

    Added new gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,67 @@
+'use strict';
+
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var maps = require('gulp-sourcemaps');
+var del = require('del');
 
-// Concatenate, minify, copy to dist folder
-gulp.task('scripts', ['concat', 'uglify'],function() {
+// Concatenate, minify, copy JavaScript file to dist folder
+gulp.task('scripts', ['minify-js']);
+
+// Minify JavaScript and place into dist folder
+gulp.task('minify-js', ['concat'], () => {
+  return gulp.src('dist/scripts/app.js')
+    .pipe(uglify())
+    .pipe(rename('all.min.js'))
+    .pipe(gulp.dest('dist/scripts'))
+});
+
+// Concatenate JavaScript files
+gulp.task('concat', () => {
+  return gulp.src('js/**/*.js')
+    .pipe(maps.init())
+    .pipe(concat('app.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('dist/scripts'));
+});
+
+// This task perform Compile Sass and Minify CSS
+gulp.task('styles', ['minify-css']);
+
+// Minify CSS
+gulp.task('minify-css', ['compile-css'], () => {
+  return gulp.src('dist/styles/*.css')
+    .pipe(maps.init())
+    .pipe(cleanCSS())
+    .pipe(maps.write())
+    .pipe(gulp.dest('dist/styles'));
+});
+
+// Compile Sass into CSS
+gulp.task('compile-css', () => {
+  return gulp.src("sass/global.scss")
+      .pipe(sass())
+      .pipe(gulp.dest('dist/styles'));
+});
+
+gulp.task('images', ['compress']);
+
+gulp.task('compress', () => {
+    console.log('image compressed');
+});
+
+gulp.task('clean', () => {
+  return del(['css', 'dist']);
+});
+
+gulp.task('build', ['scripts', 'styles', 'images'], () => {
 
 });
 
-// Concatenate
-gulp.task('concat', function() {
-  gulp.src('./js/**/*.js')
-    .pipe(concat('final.js'))
-    .pipe(gulp.dest('./js'));
-});
-
-// Minify
-gulp.task('uglify', function() {
-  console.log('uglify');
-});
-
-// Uglify
-
-// Sass compilation
-
-// Compress Images
-
-// Clean
-
-gulp.task('default', ['scripts'], function() {
-  console.log('All tasks are completed');
+gulp.task('default', ['clean'], () => {
+  gulp.start('build');
 });
